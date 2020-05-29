a4dbc9ef665a9f439168b6a49cc8b15b80e7dba3
Liam Kilcommons <liam.kilcommons27@gmail.com>
2020-05-29 12:28:49 -0600

    Added additional great circle distance algorithm (haversine) (and tests)

--- geospacepy/spherical_geometry.py
+++ geospacepy/spherical_geometry.py
@@ -3,32 +3,53 @@
 import numpy as np
 from numpy import (sin,cos,tan,arcsin,arccos,arctan2)
 
-def _azifac(degorhour):
-    if degorhour=='hour':
+def _azifac(aziunit):
+    if aziunit=='hour':
         return np.pi/12
-    elif degorhour=='deg':
+    elif aziunit=='deg':
         return np.pi/180.
+    elif aziunit=='rad':
+        return 1.
     else:
-        raise ValueError(('Invalid degorhour {}'.format(degorhour)
-                          +' valid values are deg or hour'))
+        raise ValueError(('Invalid aziunit {}'.format(degorhour)
+                          +' valid values are deg or hour or rad'))
 
-def angle_difference(ang1,ang2,degorhour='hour'):
+def angle_difference(ang1,ang2,aziunit):
     """Difference between two angles in degrees or hours (ang2-ang1),
     taking into account wrapping
+
+    PARAMETERS
+    ----------
+    
+    ang1 : float or np.ndarray
+        Angle(s) to subtract
+    ang2 : float or np.ndarray
+        Angle(s) to subtract from
+    degorhour : str, optional
+        'deg' for input angles and result in degrees
+        'hour' for input angles in hours
+
+    RETURNS
+    -------
+
+    diff : float or np.ndarray
+        Difference (ang2-ang1)
+
     """
-    ang2rad = _azifac(degorhour)
+    ang2rad = _azifac(aziunit)
     y = np.sin(ang2*ang2rad-ang1*ang2rad)
     x = np.cos(ang2*ang2rad-ang1*ang2rad)
-    return np.arctan2(y,x)/ang2rad
+    diff = np.arctan2(y,x)/ang2rad
+    return diff
 
-def angle_midpoint(ang1,ang2,degorhour='hour'):
+def angle_midpoint(ang1,ang2,aziunit):
     """
     Midpoint between two angles in degrees or hours
     """
-    return ang1 + angle_difference(ang1,ang2,degorhour=degorhour)/2.
+    return ang1 + angle_difference(ang1,ang2,aziunit)/2.
 
-def great_circle_distance(location1,location2,lonorlt='lt'):
-    """Returns n angular distances in radians between n-by-2 numpy arrays
+def _great_circle_distance(location1,location2,lonorlt='lt'):
+    """Return angular distance in radians between n-by-2 numpy arrays
     location1, location2 (calculated row-wise so diff between
     location1[0,] and location2[0,]
     assuming that these arrays have the columns lat[deg],localtime[hours]
@@ -55,49 +76,134 @@ def great_circle_distance(location1,location2,lonorlt='lt'):
         C =  np.pi - np.abs(dphi - np.pi)#get the angular distance in longitude in radians
     return np.arccos(np.cos(a)*np.cos(b)+np.sin(a)*np.sin(b)*np.cos(C))
 
-def great_circle_midpoint(location1,location2,angDist='compute',lonorlt='lt'):
+def great_circle_distance_law_of_cosines(theta1,phi1,theta2,phi2):
+    """Computes great circle distance between any number of paired 
+    locations using the law of cosines. Warning:
+    can be inaccurate for short distances. All angles in radians.
+    Greek letter convention is phi for azimuth/longitude.
     """
-    Finds the midpoint lat and lt or lon between two locations 
-    along a great circle arc. Can pass angDist as an array to speed up process 
-    if already computed, otherwise computes as needed using great_circle_distance
+    a = theta1
+    b = theta2
+    dphi = np.abs(phi2-phi1)
+    C =  np.pi - np.abs(dphi - np.pi)
+    dist = np.arccos(np.cos(a)*np.cos(b)+np.sin(a)*np.sin(b)*np.cos(C))
+    return dist
+
+def great_circle_distance_haversine_formula(theta1,phi1,theta2,phi2):
+    """Computes great circle distance between any number of paired
+    locations using the haversine formula, which is known to be accurate
+    for short distances. All angles in radians.
+    Greek letter convention is phi for azimuth/longitude
     """
-    azi2rad = np.pi/12. if lonorlt=='lt' else np.pi/180
-    wrappt = 24. if lonorlt=='lt' else 360.
-    #Bounds check
-    over = location1[:,1] > wrappt
-    under = location1[:,1] < 0.
-    location1[over,1]=location1[over,1]-wrappt
-    location1[under,1]=location1[under,1]+wrappt
+    lambda1 = np.pi/2-theta1
+    lambda2 = np.pi/2-theta2
+    delta_lambda = lambda2-lambda1
+    delta_phi = phi2-phi1
+    def hav(x):
+        """Haversine function"""
+        return np.sin(x*.5)**2.
+    def archav(theta):
+        """Inverse haversine"""
+        return 2.*np.arcsin(np.sqrt(theta))
+    def archav2(theta):
+        """Inverse haversine using arctan2"""
+        return 2.*np.arctan2(np.sqrt(theta),np.sqrt(1.-theta))
 
-    if location1.ndim == 1 or location2.ndim == 1:
-        a = (90-location1[0])*azi2rad
-        b = (90-location2[0])*azi2rad
-        if angDist is 'compute':
-            c = great_circle_distance(location1,location2,lonorlt=lonorlt)
-        else:
-            c = angDist
-        C = (location2[1]-location1[1])/24*2*np.pi
-        azi1 = location1[1]
-    else:
-        a = (90-location1[:,0])*azi2rad
-        b = (90-location2[:,0])*azi2rad
-        if angDist is 'compute':
-            c = great_circle_distance(location1,location2,lonorlt=lonorlt)
-        else:
-            c = angDist
-        C = (location2[:,1]-location1[:,1])/24*2*np.pi
-        azi1 = location1[:,1]
+    hav_dist = hav(delta_lambda)+np.cos(lambda1)*np.cos(lambda2)*hav(delta_phi)
+    
+    dist = archav2(hav_dist)
+    return dist
 
-    #original: g = arccos((cos(b)-cos(c)*cos(a)*sin(c/2))*sin(c/2)/sin(c)+cos(a)*cos(c/2))
-    cos_g = cos(a)*cos(c/2)+((cos(b)-cos(a)*cos(c))/sin(c))*sin(c/2)
-    g = arccos(cos_g)
-    sin_I = (sin(c/2)*sin(b)*sin(C)/(sin(c)*sin(g)))
-    I = arcsin(sin_I)
-    lat_mid = 90-g/(2*np.pi)*360
-    azi_mid = azi1+I/(2*np.pi)*24
-    return lat_mid, azi_mid
+def great_circle_distance(lat1,azi1,lat2,azi2,aziunit,algorithm='lawofcosines'):
+    """Computes great circle distance between any number of paired
+    locations
 
-def great_circle_rectangle_area(lats_bottom,lats_top,azis_left,azis_right,r,degorhour):
+    PARAMETERS
+    ----------
+
+    lat1 : float or np.ndarray
+        Latitude(s) of first location(s)
+    azi1 : float or np.ndarray
+        Azimuth(s) (longitude,localtime) of first location(s) in pair(s)
+    theta2 : float or np.ndarray
+        Latitude(s) of second location(s)
+    azi2 : float or np.ndarray
+        Azimuth(s) (longitude,localtime) of second location(s) in pair(s) 
+    aziunit : str
+        Unit of passed azimuth(s), valid values: hour, deg or rad
+    algorithm : str,optional
+        Which formula to use in computing the great circle distance.
+        Valid values are 'lawofcosines' or 'haversine'.
+
+    RETURNS
+    -------
+
+    dist : float or np.ndarray
+        Distance (in aziunit) between location(s) 1 and 2
+
+    """
+    algorithms = {
+                    'lawofcosines':great_circle_distance_law_of_cosines,
+                    'haversine':great_circle_distance_haversine_formula
+                }
+    if algorithm not in algorithms:
+        raise ValueError('Invalid algorithm {}'.format(algorithm)
+                        +' valid values: {}'.format([key for key in algorithms]))
+
+    azi2rad = _azifac(aziunit)
+    theta1,theta2 = np.radians(90.-lat1),np.radians(90.-lat2)
+    phi1,phi2 = azi1*azi2rad,azi2*azi2rad
+    dist = algorithms[algorithm](theta1,phi1,theta2,phi2)
+    return dist
+
+# There is currently a problem with this algorithm
+# def great_circle_midpoint(lat1,azi1,lat2,azi2,aziunit):
+#     """
+#     Finds the midpoint latitude and azimuth (longitude or localtime) between 
+#     two locations along a great circle arc.
+
+#     PARAMETERS
+#     ----------
+
+#     lat1 : float or np.ndarray
+#         Latitude(s) of first location(s)
+#     azi1 : float or np.ndarray
+#         Azimuth(s) (longitude,localtime) of first location(s) in pair(s)
+#     theta2 : float or np.ndarray
+#         Latitude(s) of second location(s)
+#     azi2 : float or np.ndarray
+#         Azimuth(s) (longitude,localtime) of second location(s) in pair(s) 
+#     aziunit : str
+#         Unit of passed azimuth(s), valid values: hour, deg or rad
+
+#     RETURNS
+#     -------
+    
+#     lat_mid : float or np.ndarray
+#         Latitude(s) of midpoint(s)
+#     azi_mid : float or np.ndarray
+#         Longitude(s) (or localtimes) of midpoint(s) (unit determined by aziunit)
+
+#     """
+#     azi2rad = _azifac(aziunit)
+
+#     a = np.radians(90.-lat1)
+#     b = np.radians(90.-lat2)
+#     c = great_circle_distance(lat1,azi1,lat2,azi2,aziunit,
+#                                 algorithm='lawofcosines')
+#     dphi = (azi2-azi1)*azi2rad
+#     C =  np.pi - np.abs(dphi - np.pi)
+    
+#     #original: g = arccos((cos(b)-cos(c)*cos(a)*sin(c/2))*sin(c/2)/sin(c)+cos(a)*cos(c/2))
+#     cos_g = cos(a)*cos(c/2)+((cos(b)-cos(a)*cos(c))/sin(c))*sin(c/2)
+#     g = arccos(cos_g)
+#     sin_I = (sin(c/2)*sin(b)*sin(C)/(sin(c)*sin(g)))
+#     I = arcsin(sin_I)
+#     lat_mid = 90.-np.degrees(g)
+#     azi_mid = azi1+I/azi2rad
+#     return lat_mid, azi_mid
+
+def great_circle_rectangle_area(lats_bottom,lats_top,azis_left,azis_right,r,aziunit):
     """Calculate the surface area of any number of 'great-circle rectangles'
     (spherical quadrilaterals, whose 4 sides are all great circle arcs) 
     on the surface of a sphere of radius r.
@@ -118,9 +224,9 @@ def great_circle_rectangle_area(lats_bottom,lats_top,azis_left,azis_right,r,dego
             of the great-circle rectangle(s)
         r - float or np.ndarray
             Radius of the sphere on which to calculate the area
-        degorhour - str
+        aziunit - str
             Units of the azimuth/longitude ('deg' for degrees,
-            'hour' for hours)
+            'hour' for hours, 'rad' for radians)
 
     RETURNS
     -------
@@ -129,16 +235,16 @@ def great_circle_rectangle_area(lats_bottom,lats_top,azis_left,azis_right,r,dego
             Surface areas of the great circle rectangles
 
     """
-    ang2rad = _azifac(degorhour)
+    azi2rad = _azifac(aziunit)
 
     if np.any((lats_top-lats_bottom) < 0):
         raise ValueError(('Latitudes for bottom edge cannot be > '
                           +'latitudes for top edge'))
     
-    dazi = angle_difference(azis_left,azis_right,degorhour=degorhour)
-    dazi = np.mod(dazi,2*np.pi/ang2rad)
+    dazi = angle_difference(azis_left,azis_right,aziunit)
+    dazi = np.mod(dazi,2*np.pi/azi2rad)
 
-    if np.any(dazi*ang2rad>np.pi):
+    if np.any(dazi*azi2rad>np.pi):
         raise ValueError(('Angles > 180 between longitudes for the right edge'
                           +' and longitudes for the left edge were found.'
                           +' usually this means the right and left edge inputs'
@@ -146,11 +252,11 @@ def great_circle_rectangle_area(lats_bottom,lats_top,azis_left,azis_right,r,dego
 
     theta_top = (90.-lats_top)*np.pi/180. #theta / lat - converted to radians
     theta_bottom = (90.-lats_bottom)*np.pi/180. #theta / lat - converted to radians
-    dphi = np.abs(dazi)*ang2rad #delta phi / lon - converted to radians
+    dphi = np.abs(dazi)*azi2rad #delta phi / lon - converted to radians
     areas = np.abs(r**2*dphi*(np.cos(theta_top)-np.cos(theta_bottom)))
     return areas
 
-def grid_surface_integral(grid_lats,grid_azis,grid_values,sphere_radius,degorhour):
+def grid_surface_integral(grid_lats,grid_azis,grid_values,sphere_radius,aziunit):
     """Calculate the approximate surface integral of a field of values
     specified on a grid of m latitudes and n longitudes
 
@@ -168,6 +274,9 @@ def grid_surface_integral(grid_lats,grid_azis,grid_values,sphere_radius,degorhou
         sphere_radius - float
             The radius of the sphere on which the surface integral will
             be computed
+        aziunit - str
+            The unit of the azimuthal angles ('deg' for longitude, 'hour' for
+            localtimes)
 
     RETURNS
     -------
@@ -192,13 +301,13 @@ def grid_surface_integral(grid_lats,grid_azis,grid_values,sphere_radius,degorhou
     dlat = np.abs(np.nanmedian(dlats))
 
     azis = grid_azis[0,:]
-    dazis = angle_difference(azis[:-1],azis[1:],degorhour=degorhour)
-    dazis = np.mod(dazis,2*np.pi/_azifac(degorhour)) #Ensure > 0
+    dazis = angle_difference(azis[:-1],azis[1:],aziunit)
+    dazis = np.mod(dazis,2*np.pi/_azifac(aziunit)) #Ensure > 0
     dazi = np.nanmedian(dazis)
 
     gridcell_bottom_lats = grid_lats-dlat/2.
     gridcell_top_lats = grid_lats+dlat/2.
-    gridcell_left_azis = angle_difference(dazi/2.,grid_azis,degorhour=degorhour)
+    gridcell_left_azis = angle_difference(dazi/2.,grid_azis,aziunit)
     gridcell_right_azis = grid_azis+dazi/2.
     
     gridcell_areas = great_circle_rectangle_area(gridcell_bottom_lats,
@@ -206,7 +315,7 @@ def grid_surface_integral(grid_lats,grid_azis,grid_values,sphere_radius,degorhou
                                                     gridcell_left_azis,
                                                     gridcell_right_azis,
                                                     sphere_radius,
-                                                    degorhour)
+                                                    aziunit)
 
     integrated_values = np.nansum(gridcell_areas*grid_values)
     return integrated_values
--- geospacepy/test/test_spherical_geometry.py
+++ geospacepy/test/test_spherical_geometry.py
@@ -4,7 +4,9 @@ import pytest
 import numpy as np
 import numpy.testing as nptest
 
-from geospacepy.spherical_geometry import angle_difference,grid_surface_integral
+from geospacepy.spherical_geometry import (angle_difference,
+                                            grid_surface_integral,
+                                            great_circle_distance)
 
 def test_angle_difference_across_zero():
     ang2 = 10.
@@ -13,6 +15,54 @@ def test_angle_difference_across_zero():
     expected_difference = 20.
     difference = angle_difference(ang1,ang2,'deg')
     assert(np.abs(expected_difference-difference)<.001)
+
+@pytest.mark.parametrize('lat1,lat2,lon,algorithm',[(-85.,85.,10,'lawofcosines'),
+                                                    (-85.,85.,10,'haversine'),
+                                                    (50.,60.,10,'lawofcosines'),
+                                                    (50.,60.,10,'haversine'),
+                                                    (50.,50.001,10,'lawofcosines'),
+                                                    (50.,50.001,10,'haversine')])
+def test_great_circle_distance_on_meridian(lat1,lat2,lon,algorithm):
+    dist_expected = np.radians(np.abs(lat2-lat1))
+    dist = great_circle_distance(lat1,lon,lat2,lon,'deg',algorithm=algorithm)
+    pertol = .2
+    percent_error = np.abs(dist-dist_expected)/dist_expected*100.
+    assert percent_error<pertol
+
+@pytest.mark.parametrize('lon1,lon2,algorithm',[(-30.,140.,'lawofcosines'),
+                                                (-30.,140.,'haversine'),
+                                                (50.,80.,'lawofcosines'),
+                                                (50.,80.,'haversine'),
+                                                (50.,50.001,'lawofcosines'),
+                                                (50.,50.001,'haversine')])
+def test_great_circle_distance_on_equator(lon1,lon2,algorithm):
+    dist_expected = np.radians(np.abs(lon2-lon1))
+    dist = great_circle_distance(1.,lon1,1.,lon2,'deg',algorithm=algorithm)
+    pertol = .2
+    percent_error = np.abs(dist-dist_expected)/dist_expected*100. 
+    assert percent_error<pertol
+
+# @pytest.mark.parametrize('lat1,lat2,lon',[(50.,60.,10)])
+# def test_great_circle_midpoint_on_meridian(lat1,lat2,lon):
+#     lat_mid_expected = lat1+angle_difference(lat1,lat2,'deg')
+#     lon_mid_expected = lon
+
+#     lat_mid,lon_mid = great_circle_midpoint(lat1,lon,lat2,lon,'deg')
+#     tol = 1e-6
+#     assert np.abs(lat_mid-lat_mid_expected)<tol
+#     assert np.abs(lon_mid-lon_mid_expected)<tol
+
+@pytest.mark.parametrize('lat1,lon1,lat2,lon2',[(50.,10.,60.,20),
+                                               (50.,10.,50.001,10.001),
+                                               (50.,10.,50.0001,10.0001),])
+def test_haversine_and_lawofcosines_produce_same_great_circle_distance(lat1,lon1,lat2,lon2):    
+    dist_haversine = great_circle_distance(lat1,lon1,lat2,lon2,'deg',
+                                            algorithm='haversine')
+    dist_lawofcosines = great_circle_distance(lat1,lon1,lat2,lon2,'deg',
+                                                algorithm='lawofcosines')
+    pertol = .003
+    percent_difference = np.abs(1.-dist_haversine/dist_lawofcosines)*100.
+    assert percent_difference < pertol
 
 def test_grid_surface_integral_of_half_unit_sphere_is_2pi():
     lats = [15.,45.,75.]
@@ -27,3 +77,6 @@ def test_grid_surface_integral_of_half_unit_sphere_is_2pi():
                                                 1.,'deg')
     tol = .001
     assert(np.abs(integrated_values-expected_integrated_values)<tol)
+
+
+
